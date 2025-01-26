0517b48002c7eaa9c6b29476938481ec2689cc47
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2025-01-25 22:41:17 -0500

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,20 @@
+
+#create a dictionary
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+#get built-in function can be used as key
+month = input("Enter month: ")
+print(monthConversions.get(month, "Not valid key"))
